508d0111b8bf4c8bcd6ff0fdccfd57ed72a5aaf7
yudhacm <156794837+yudhacm@users.noreply.github.com>
2025-11-09 22:07:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os  # ✅ harus ada
+BASE_DIR = os.path.dirname(__file__)  # ✅ harus ada
+
 import streamlit as st
 import joblib
 import numpy as np
@@ -15,52 +18,42 @@ def load_model():
 
 model, scaler, le = load_model()
 
-# ================== UI HEADER ==================
 st.title("🎤 Voice Command Recognition")
 st.write("Ucapkan **buka** atau **tutup**")
 
-# ================== MIC RECORD ==================
 audio = mic_recorder(start_prompt="🎙 Mulai Rekam", stop_prompt="⏹ Stop Rekam", just_once=True)
 
 if audio and "bytes" in audio:
 
-    # --- 1. SAVE AUDIO BYTE TO WAV ---
     with wave.open("temp.wav", "wb") as wf:
-        wf.setnchannels(1)                # mono
-        wf.setsampwidth(2)                # 16 bit
-        wf.setframerate(16000)            # 16 kHz
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(16000)
         wf.writeframes(audio["bytes"])
 
-    # --- 2. LOAD WAV USING SCIPY ---
     sr, y = wavfile.read("temp.wav")
     y = y.astype(np.float32)
 
-    # --- 3. NORMALIZE ---
     if np.max(np.abs(y)) > 0:
         y = y / np.max(np.abs(y))
 
-    # --- 4. FORCE 1 SECOND (16000 samples) ---
     SAMPLES = 16000
     if len(y) > SAMPLES:
         y = y[:SAMPLES]
     else:
         y = np.pad(y, (0, SAMPLES - len(y)))
 
-    # --- 5. PLAY AUDIO UI ---
     st.audio("temp.wav", format="audio/wav")
 
-    # --- 6. EXTRACT MFCC (No librosa) ---
     feat = mfcc(y, samplerate=16000, numcep=13)
     feat = np.mean(feat, axis=0).reshape(1, -1)
     feat = scaler.transform(feat)
 
-    # --- 7. PREDICTION ---
     prob = model.predict_proba(feat)[0]
     pred = model.predict(feat)[0]
     label = le.inverse_transform([pred])[0]
     conf = max(prob) * 100
 
-    # --- 8. RESULT OUTPUT ---
     st.subheader("🔍 Hasil Prediksi")
 
     if conf >= 70:
